5bdc86945c806a4435de01174b26776911e2f72f
Oskar Kraak <74422644+oskarkraak@users.noreply.github.com>
2022-10-14 13:27:42 +0200

    Add main method

---
+++ src/main/java/com/oskarkraak/bundestagvotescraper/BundestagVoteScraper.java
@@ -0,0 +1,13 @@
+package com.oskarkraak.bundestagvotescraper;
+
+public class BundestagVoteScraper {
+
+    public static void main(String[] args) {
+
+    }
+
+    public BundestagVoteScraper() {
+
+    }
+
+}
