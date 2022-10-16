11926c0bb2c229c6c30d40360aa75dd9e7723e47
Oskar Kraak <74422644+oskarkraak@users.noreply.github.com>
2022-10-16 22:38:33 +0200

    Define HTML API

---
+++ src/main/java/com/oskarkraak/bundestagvotescraper/HTML.java
@@ -0,0 +1,31 @@
+package com.oskarkraak.bundestagvotescraper;
+
+public class HTML {
+
+    String html;
+
+    public HTML(String html) {
+        this.html = html;
+    }
+
+    public static HTML get(String url) {
+        // TODO
+        return null;
+    }
+
+    public String[] getRootTags() {
+        // TODO
+        return null;
+    }
+
+    public String getTagContent(String tag) {
+        // TODO
+        return "";
+    }
+
+    public String getTagProperty(String tag, String propertyName) {
+        // TODO
+        return "";
+    }
+
+}
