07d9416f3803c3d61c58b75047857b0d098d508f
Oskar Kraak <74422644+oskarkraak@users.noreply.github.com>
2022-10-16 21:17:13 +0200

    Remove origin field from Vote
    Description format is inconsistent so origin cannot be extracted reliably

--- src/main/java/com/oskarkraak/bundestagvotescraper/Vote.java
+++ src/main/java/com/oskarkraak/bundestagvotescraper/Vote.java
@@ -4,7 +4,6 @@ public class Vote {
 
     private final String date;
     private final String name;
-    private final String origin;
     private final String publishingDate;
     private final XLSX results;
 
@@ -13,13 +12,11 @@ public class Vote {
         this.results = results;
         date = getDateFromDescription(documentDescription);
         name = getNameFromDescription(documentDescription);
-        origin = getOriginFromDescription(documentDescription);
     }
 
-    public Vote(String publishingDate, String date, String name, String origin, XLSX results) {
+    public Vote(String publishingDate, String date, String name, XLSX results) {
         this.date = date;
         this.name = name;
-        this.origin = origin;
         this.publishingDate = publishingDate;
         this.results = results;
     }
@@ -34,21 +31,12 @@ public class Vote {
         return "";
     }
 
-    private String getOriginFromDescription(String description) {
-        // TODO
-        return "";
-    }
-
     public String getDate() {
         return date;
     }
 
     public String getName() {
         return name;
-    }
-
-    public String getOrigin() {
-        return origin;
     }
 
     public String getPublishingDate() {
