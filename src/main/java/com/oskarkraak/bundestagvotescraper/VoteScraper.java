c521af3686a43c6143959ef83aec03be3b8ba0db
Oskar Kraak <74422644+oskarkraak@users.noreply.github.com>
2022-10-18 21:03:52 +0200

    Add downloading vote results

--- src/main/java/com/oskarkraak/bundestagvotescraper/VoteScraper.java
+++ src/main/java/com/oskarkraak/bundestagvotescraper/VoteScraper.java
@@ -71,7 +71,21 @@ public class VoteScraper implements Iterable<Vote> {
                         .getContent().getTags()[0]
                         .getContent().getTags()[0]
                         .getContent().toString();
-                XLSX results = null; // TODO get XLSX link
+                String xlsxLink = null;
+                HTML.Tag[] downloadLinks = voteTag[2]
+                        .getContent().getTags()[0]
+                        .getContent().getTags()[1]
+                        .getContent().getTags();
+                for (HTML.Tag link : downloadLinks)
+                    if (link.getContent().getTags()[0].getProperty("title").startsWith("XLSX"))
+                        xlsxLink = link.getContent().getTags()[0].getProperty("href");
+                XLSX results;
+                try {
+                    results = XLSX.download(BUNDESTAG_DOMAIN + xlsxLink);
+                } catch (IOException e) {
+                    results = null;
+                    e.printStackTrace();
+                }
                 votes[i] = new Vote(publishingDate, documentDescription, results);
             }
             return votes;
